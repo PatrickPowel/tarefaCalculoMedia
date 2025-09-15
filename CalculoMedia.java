fa4ffb2f53a04aa0aa2c6eaf1907bfc03984e92a
Patrick Willians Powel <61129859+PatrickPowel@users.noreply.github.com>
2025-09-14 22:33:34 -0300

    Caclculo media
    calculando as notas para obter uma media

---
+++ CalculoMedia.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class CalculoMedia {
+    public static void main(String[] args){
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Digite a nota 1: ");
+        double nota1 = entrada.nextDouble();
+        System.out.println("Digite a nota 2: ");
+        double nota2 = entrada.nextDouble();
+        System.out.println("Digite a nota 3: ");
+        double nota3 = entrada.nextDouble();
+        System.out.println("Digite a nota 4: ");
+        double nota4 = entrada.nextDouble();
+        double media = nota1 + nota2 + nota3 + nota4 / 4;
+        System.out.println(media);
+        entrada.close();
+    }
+}
